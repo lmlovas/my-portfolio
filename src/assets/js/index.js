5f38a3a265cdc2f0466fe0958e7c85f4daac722b
lmlovas <lorraine.lovas@gmail.com>
2019-11-13 16:32:20 +0100

    added background image

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,7 @@
 const style = [
-    'background: #d60161',
-    'color: #fff',
-    'padding: 10px 20px',
-    'line-height: 35px',
-  ].join(';');
-  console.log('%c Hello, friend! ♥️', style);
+  "background: #d60161",
+  "color: #fff",
+  "padding: 10px 20px",
+  "line-height: 35px"
+].join(";");
+console.log("%c Hello, friend! ♥️", style);
